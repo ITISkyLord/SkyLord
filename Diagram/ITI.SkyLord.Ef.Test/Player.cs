94dadf3afddc06cf81c3db52d3e97b2fbec018d6
Loic <donne@intechinfo.fr>
2015-11-04 15:35:04 +0100

    Add test :
    Create Player
    DisplayPlayers

---
+++ Diagram/ITI.SkyLord.Ef.Test/Player.cs
@@ -0,0 +1,36 @@
+﻿using ITI.SkyLord.DAL.Contexts.GameModel;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.SkyLord.Ef.Test
+{
+    [TestFixture]
+    public class Player
+    {
+        [Test]
+        public void CreatePlayer()
+        {
+            using (GameEntity context = new GameEntity())
+            {
+                context.Players.Add(new Players { Name = "Boss" });
+                context.SaveChanges();
+            }
+        }
+
+        [Test]
+        public void DisplayPlayers()
+        {
+            using (GameEntity context = new GameEntity())
+            {
+                foreach(Players P in context.Players)
+                {
+                    Console.WriteLine("Name: {0}", P.Name);
+                }
+            }
+        }
+    }
+}
