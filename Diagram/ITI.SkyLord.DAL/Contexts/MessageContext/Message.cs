8519024c6efbc65c3bd5a29028d99435bc88ed1d
Loic <donne@intechinfo.fr>
2015-11-09 15:32:33 +0100

    Implement MessageContext

---
+++ Diagram/ITI.SkyLord.DAL/Contexts/MessageContext/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.SkyLord.DAL.Contexts.MessageContext
+{
+   public class Message
+    {
+        public long MessageId { get; set; }
+        public string MessageObject { get; set; }
+        public string CoreMessage { get; set; }
+        public PlayerForMessageContext Receiver { get; set; }
+        public PlayerForMessageContext Sender { get; set; }
+    }
+}
