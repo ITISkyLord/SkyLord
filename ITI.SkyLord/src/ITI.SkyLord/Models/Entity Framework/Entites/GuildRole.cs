1c9fe4a895c2d9a021d3218eb84920be76b93431
Loic <donne@intechinfo.fr>
2015-11-23 10:02:35 +0100

    Changement get set Loic

--- ITI.SkyLord/src/ITI.SkyLord/Models/Entity Framework/Entites/GuildRole.cs
+++ ITI.SkyLord/src/ITI.SkyLord/Models/Entity Framework/Entites/GuildRole.cs
@@ -8,14 +8,36 @@ namespace ITI.SkyLord
 {
     public class GuildRole
     {
-        private long _guildRoleId;
         private string _name;
         private List<MemberRight> _rights; //Rajouter des rôles dans MemberRight si nécessaire.
 
         [Key]
         public long GuildRoleId { get; set; }
-        public string Name { get; set; }
-        public List<MemberRight> Rights { get; set; }
 
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public List<MemberRight> Rights
+        {
+            get
+            {
+                return _rights;
+            }
+
+            set
+            {
+                _rights = value;
+            }
+        }
     }
 }
