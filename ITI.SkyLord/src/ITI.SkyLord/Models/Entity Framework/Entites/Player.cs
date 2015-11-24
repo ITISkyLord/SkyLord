b6ca5ae51e86048af86c080d898fa08316b7db7c
Erwan de Bénazé <duhildebenaze@intechinfo.fr>
2015-11-24 14:31:48 +0100

    Des trucs en plus

--- ITI.SkyLord/src/ITI.SkyLord/Models/Entity Framework/Entites/Player.cs
+++ ITI.SkyLord/src/ITI.SkyLord/Models/Entity Framework/Entites/Player.cs
@@ -33,7 +33,7 @@ namespace ITI.SkyLord
             if (world == null) throw new ArgumentNullException("world is null.");
             if (!IsValidEmailStatic.IsValidEmail(mail)) throw new ArgumentException("Invalid mail");
             if (name.Length > 50) throw new ArgumentOutOfRangeException(" Maximum lenght of name is 50. Your name lenght is " + name.Length + " at the moment.");
-            if (password.Length > 75) throw new ArgumentOutOfRangeException(" Maximum lenght of password is 75. Your name lenght is " + password.Length + " at the moment.");
+            if (password.Length > 1000) throw new ArgumentOutOfRangeException(" Maximum lenght of password is 1000. Your password lenght is " + password.Length + " at the moment.");
             this.Name = name;
             this.World = world;
             this.Islands = new List<Island>();
