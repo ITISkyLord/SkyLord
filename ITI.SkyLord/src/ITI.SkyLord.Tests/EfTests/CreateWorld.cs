a0d6973d266def6bd9816480072780cb93343e6c
Tristan Letrou <letrou@intechinfo.fr>
2015-11-30 14:15:44 +0100

    Inscription + Login + Cookie PlayerId
    Pour accéder au playerId : Request.Cookies["PlayerId"]

--- ITI.SkyLord/src/ITI.SkyLord.Tests/EfTests/CreateWorld.cs
+++ ITI.SkyLord/src/ITI.SkyLord.Tests/EfTests/CreateWorld.cs
@@ -10,37 +10,37 @@ namespace ITI.SkyLord.Tests.EfTests
     [TestFixture]
     public class CreateWorld
     {
-        [Test]
-        public void Create_world()
-        {
-            using (WorldContext context = new WorldContext())
-            {
-                World world = new World();
-                context.Add(world);
-                context.Add(world.Map);
-                context.SaveChanges();
-            }
-            Console.WriteLine("New world create !");
-        }
+        //[Test]
+        //public void Create_world()
+        //{
+        //    using (WorldContext context = new WorldContext())
+        //    {
+        //        World world = new World();
+        //        context.Add(world);
+        //        context.Add(world.Map);
+        //        context.SaveChanges();
+        //    }
+        //    Console.WriteLine("New world create !");
+        //}
 
-        [Test]
-        public void Get_world()
-        {
-            using (WorldContext context = new WorldContext())
-            {
-                Assert.That(context.Worlds.Count() != 0);            
-            }
-        }
+        //[Test]
+        //public void Get_world()
+        //{
+        //    using (WorldContext context = new WorldContext())
+        //    {
+        //        Assert.That(context.Worlds.Count() != 0);            
+        //    }
+        //}
 
-        [Test]
-        public void Remove_world()
-        {
-            using (WorldContext context = new WorldContext())
-            {
-                World w = context.Worlds.FirstOrDefault();
-                context.Worlds.Remove(w);
-                Console.WriteLine("The world is remove sir ! ");
-            }
-        }
+        //[Test]
+        //public void Remove_world()
+        //{
+        //    using (WorldContext context = new WorldContext())
+        //    {
+        //        World w = context.Worlds.FirstOrDefault();
+        //        context.Worlds.Remove(w);
+        //        Console.WriteLine("The world is remove sir ! ");
+        //    }
+        //}
     }
 }
--- ITI.SkyLord/src/ITI.SkyLord.Tests/EfTests/PlayerTests.cs
+++ ITI.SkyLord/src/ITI.SkyLord.Tests/EfTests/PlayerTests.cs
@@ -1,54 +1,74 @@
-﻿//using ITI.SkyLord.Models.Entity_Framework.Contexts;
-//using Microsoft.Data.Entity;
-//using NUnit.Framework;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
+﻿using ITI.SkyLord.Models.Entity_Framework.Contexts;
+using Microsoft.Data.Entity;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-//namespace ITI.SkyLord.Tests.EfTests
-//{
-//    [TestFixture]
-//    public class PlayerTests
-//    {
-//        World _world;
+namespace ITI.SkyLord.Tests.EfTests
+{
+    [TestFixture]
+    public class PlayerTests
+    {
+        World _world;
+        public PlayerTests()
+        {
+            using ( PlayerContext context = new PlayerContext() )
+            {
+                _world = context.GetWorld();
+            }
 
-//        [Test]
-//        public void Add2Players()
-//        {
-//            PlayerContext context = new PlayerContext();
-//            _world = new World();
-//            Player testPlayer = new Player(_world, "Loic");
-//            Player testPlayer2 = new Player(_world, "Kevin");
- 
-//            context.AddPlayer(testPlayer); context.AddPlayer(testPlayer2);
-//            Console.WriteLine("Player saved");
-//        }
+            //using ( PlayerContext context = new PlayerContext() )
+            //{
+            //    context.Add( _world );
+            //    context.SaveChanges();
+            //}
+        }
 
-//        [Test]
-//        public void DisplayPlayers()
-//        {
-//            using( PlayerContext context = new PlayerContext())
-//            {
-//                foreach( Player p in context.Players)
-//                {
-//                    Console.WriteLine("Name: {0}", p.Name);
-//                }
-//            }
-//        }
+        [Test]
+        public void Create_a_new_player_and_save_it()
+        {
+            Player p = new Player( _world, "Thanur", "toto@intechinfo.fr", "toto" );
+            Player playerFromDatabase;
 
-//        [Test]
-//        public void DeletePlayers()
-//        {
-//            using (PlayerContext context = new PlayerContext())
-//            {
-//                foreach(Player p in context.Players)
-//                {
-//                    context.Players.Remove(p);
-//                }
-//                Console.WriteLine("Voici le nombre d'entrée maintenant dans la bdd : " + context.Players.Count());
-//            }
-            
-//        }
-//    }
-//}
+            using ( PlayerContext context = new PlayerContext() )
+            {
+                context.AddPlayer( p );
+
+                playerFromDatabase = context.FindPlayer( p.Profil.Mail );
+            }
+
+            Assert.That( playerFromDatabase.Name == p.Name && playerFromDatabase.Profil.Mail == p.Profil.Mail );
+
+            using ( PlayerContext context = new PlayerContext() )
+            {
+                context.RemovePlayer( p.PlayerId );
+            }
+
+
+        }
+
+        [Test]
+        public void DisplayPlayers()
+        {
+            Player p1 = new Player( _world, "Thanur", "toto@intechinfo.fr", "toto" );
+            Player p2 = new Player( _world, "MachinTruc", "machin@intechinfo.fr", "toto" );
+
+            using ( PlayerContext context = new PlayerContext() )
+            {
+                
+                context.AddPlayer( p1 );
+                context.AddPlayer( p2 );
+
+                foreach ( Player player in context.Players.Include( p => p.Profil ) )
+                {
+                    Console.WriteLine( "Name: {0}, Mail : {1}", player.Name, player.Profil.Mail );
+                }
+
+                context.RemovePlayer( p1.PlayerId );
+                context.RemovePlayer( p2.PlayerId );
+            }
+        }
+    }
+}
