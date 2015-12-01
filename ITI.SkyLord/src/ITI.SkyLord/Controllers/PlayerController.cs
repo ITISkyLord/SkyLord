c394602d7f4ceb883f68b367f926cc002dfff919
Loic <donne@intechinfo.fr>
2015-12-01 15:18:39 +0100

    Ajout du Controller SeePlayers

--- ITI.SkyLord/src/ITI.SkyLord/Controllers/PlayerController.cs
+++ ITI.SkyLord/src/ITI.SkyLord/Controllers/PlayerController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNet.Mvc;
+﻿using ITI.SkyLord.Models.Entity_Framework.Contexts;
+using ITI.SkyLord.ViewModel.SeePlayers;
+using Microsoft.AspNet.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,13 +10,26 @@ namespace ITI.SkyLord.Controllers
 {
     public class PlayerController : Controller
     {
+        [FromServices]
+        public PlayerContext PlayerContext { get; set; }
+
         public IActionResult Index()
         {
             return View();
         }
         public IActionResult SeePlayers()
         {
-            return View();
+
+            Player playerActif = PlayerContext.Players.Where(p => p.Name == "Marvin").SingleOrDefault();
+
+            List<Player> players = PlayerContext.Players.ToList();
+            players.Remove(playerActif);
+
+            SeePlayers sp = new SeePlayers();
+
+            sp.Players = players;
+            sp.ActivePlayer = playerActif;
+            return View(sp);
         }
     }
 }
---
+++ ITI.SkyLord/src/ITI.SkyLord/ViewModel/SeePlayers/SeePlayers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITI.SkyLord.ViewModel.SeePlayers
+{
+    public class SeePlayers
+    {
+        public List<Player> Players { get; set; }
+         public Player ActivePlayer { get; set; }
+    }
+}
