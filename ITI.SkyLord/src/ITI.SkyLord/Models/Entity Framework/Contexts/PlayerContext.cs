11094f1df0ebbf98a4d3da398c411270b6956998
Tristan Letrou <letrou@intechinfo.fr>
2015-12-07 09:41:56 +0100

    Suppression de commentaires

--- ITI.SkyLord/src/ITI.SkyLord/Models/Entity Framework/Contexts/PlayerContext.cs
+++ ITI.SkyLord/src/ITI.SkyLord/Models/Entity Framework/Contexts/PlayerContext.cs
@@ -16,11 +16,6 @@ namespace ITI.SkyLord.Models.Entity_Framework.Contexts
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
-
-            //builder.Entity<Player>()
-            //    .HasOne( p => p.Profil )
-            //    .WithOne( pr => pr.Player )
-            //    .OnDelete( DeleteBehavior.Cascade );
         }
         public IConfigurationRoot Configuration { get; set; }
 
