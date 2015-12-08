29fd8f9d3658490ea5fd470ccb771da4667413c6
Tristan Letrou <letrou@intechinfo.fr>
2015-12-08 09:43:01 +0100

    Méthodes de test AddPlayer / RemovePlayer

--- ITI.SkyLord/src/ITI.SkyLord.Tests/SeeOtherPlayers.cs
+++ ITI.SkyLord/src/ITI.SkyLord.Tests/SeeOtherPlayers.cs
@@ -21,29 +21,29 @@ namespace ITI.SkyLord.Tests
             }
         }
 
-        [Test]
-        public void See_information_of_a_player()
-        {
-            Player p1 = new Player { World = _world, Name = "Marvin", Mail = "marvin@cdk.fr", Password = "tought" };
+        //[Test]
+        //public void See_information_of_a_player()
+        //{
+        //    Player p1 = new Player { World = _world, Name = "Marvin", Mail = "marvin@cdk.fr", Password = "tought" };
 
-            using (PlayerContext pc = new PlayerContext())
-            {
-                try
-                {
-                    pc.AddPlayer(p1);
-                    pc.SaveChanges();
+        //    using (PlayerContext pc = new PlayerContext())
+        //    {
+        //        try
+        //        {
+        //            pc.AddPlayer(p1);
+        //            pc.SaveChanges();
 
-                    Player x = pc.Players.Include(p => p.Profil).First(o => o.Name == "Marvin");
+        //            Player x = pc.Players.Include(p => p.Profil).First(o => o.Name == "Marvin");
 
-                    Console.WriteLine("Name : {0}, Password : {1}, Description {2}", x.Name, x.Password, x.Profil.Description);
-                }
-                finally
-                {
-                    pc.RemovePlayer(p1.PlayerId);
-                    pc.SaveChanges();
-                }
+        //            Console.WriteLine("Name : {0}, Password : {1}, Description {2}", x.Name, x.Password, x.Profil.Description);
+        //        }
+        //        finally
+        //        {
+        //            pc.RemovePlayer(p1.PlayerId);
+        //            pc.SaveChanges();
+        //        }
   
-            }
-        }
+        //    }
+        //}
     }
 }
