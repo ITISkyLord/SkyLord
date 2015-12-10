929d89f0b71d02e043487c86ac0d742f1cf3d110
Loic <donne@intechinfo.fr>
2015-12-10 22:49:22 +0100

    Test sur la gestion des ressources : Ajout + retrait de ressources

--- ITI.SkyLord/src/ITI.SkyLord.Tests/EfTests/RessourceTest.cs
+++ ITI.SkyLord/src/ITI.SkyLord.Tests/EfTests/RessourceTest.cs
@@ -42,12 +42,60 @@ namespace ITI.SkyLord.Tests.EfTests
         {
             using (PlayerContext context = new PlayerContext())
             {
-                Player playerActive = context.Players.Where(p => p.Name == "LoicD").SingleOrDefault();
+                Player playerActive = context.Players.Include(pro => pro.Profil).Include(isl => isl.Islands).Include(w => w.World).Where(p => p.Name == "LoicD").SingleOrDefault();
                 using (IslandContext islandContext = new IslandContext())
                 {
+                    int quantRessource = 10;
                     Ressource ressource = islandContext.Islands.Include(r => r.AllRessources).Where(i => i.Owner.PlayerId == playerActive.PlayerId).Select(res => res.AllRessources).SingleOrDefault();
+
+                    ressource.ChangeCristal(quantRessource);
+                    ressource.ChangeMagic(quantRessource);
+                    ressource.ChangeMetal(quantRessource);
+                    ressource.ChangeWood(quantRessource);
+                    islandContext.SaveChanges();
+
+                    Console.WriteLine("Les ressources de cristal ajoutés : " + ressource.Cristal.ToString());
+                    Console.WriteLine("Les ressources de magie ajoutés : "+ ressource.Magic.ToString());
+                    Console.WriteLine("Les ressources de metal ajoutés : " + ressource.Metal.ToString());
+                    Console.WriteLine("Les ressources de bois ajoutés : " + ressource.Wood.ToString());
+
+                    ressource.WithdrawCristal(quantRessource);
+                    ressource.WithdrawMagic(quantRessource);
+                    ressource.WithdrawMetal(quantRessource);
+                    ressource.WithdrawWood(quantRessource);
+                    islandContext.SaveChanges();
+
+                    Console.WriteLine(ressource.Cristal.ToString());
+                    Console.WriteLine(ressource.Magic.ToString());
+                    Console.WriteLine(ressource.Metal.ToString());
+                    Console.WriteLine(ressource.Wood.ToString());
+
                 }     
             }               
+        }
+        [Test]
+        public void WithdrawRessources()
+        {
+            using (PlayerContext context = new PlayerContext())
+            {
+                Player playerActive = context.Players.Include(pro => pro.Profil).Include(isl => isl.Islands).Include(w => w.World).Where(p => p.Name == "LoicD").SingleOrDefault();
+                using (IslandContext islandContext = new IslandContext())
+                {
+                    int quantRessource = 50;
+                    Ressource ressource = islandContext.Islands.Include(r => r.AllRessources).Where(i => i.Owner.PlayerId == playerActive.PlayerId).Select(res => res.AllRessources).SingleOrDefault();
+
+                    ressource.WithdrawCristal(quantRessource);
+                    ressource.WithdrawMagic(quantRessource);
+                    ressource.WithdrawMetal(quantRessource);
+                    ressource.WithdrawWood(quantRessource);
+                    islandContext.SaveChanges();
+
+                    Console.WriteLine("Les ressources de cristals sont de maintenant : " + ressource.Cristal.ToString());
+                    Console.WriteLine("Les ressources de magies sont de maintenant : " + ressource.Magic.ToString());
+                    Console.WriteLine("Les ressources de metal sont de maintenant : " + ressource.Metal.ToString());
+                    Console.WriteLine("Les ressources de bois sont de maintenant : " + ressource.Wood.ToString());
+                }
+            }
         }
     }
 }
