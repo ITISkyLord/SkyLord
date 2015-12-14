1d240eb2d4f63ef6636caba05ff9cbe46f925baa
Loic <donne@intechinfo.fr>
2015-12-14 11:48:03 +0100

    Correction de SeePlayers et SeeInformationOfAnPlayer

--- ITI.SkyLord/src/ITI.SkyLord/ViewModel/SeePlayers/SeePlayersViewModel.cs
+++ ITI.SkyLord/src/ITI.SkyLord/ViewModel/SeePlayers/SeePlayersViewModel.cs
@@ -5,9 +5,15 @@ using System.Threading.Tasks;
 
 namespace ITI.SkyLord.ViewModel.SeePlayers
 {
-    public class SeePlayers
+    public class SeePlayersViewModel
     {
+        /// <summary>
+        /// Require Island(Require Coordoniate)
+        /// </summary>
         public List<Player> Players { get; set; }
+
         public Player ActivePlayer { get; set; }
+
+        public List<Island> IslandsOfOthersPlayers { get; set; }
     }
 }
