8db12cd25cb9f366886c2fb613d76b6ed80dbb08
KevinRoussel <roussel@intechinfo.fr>
2015-12-15 10:06:15 +0100

    Mise en place du StandardVM

---
+++ ITI.SkyLord/src/ITI.SkyLord/ViewModel/StandardViewModel.cs
@@ -0,0 +1,27 @@
+﻿using ITI.SkyLord.Models.Entity_Framework.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITI.SkyLord.ViewModel
+{
+    public class StandardViewModel
+    {
+        public StandardViewModel(int playerId, int currentIslandId)
+        {
+
+            using (var armyContext = new ArmyContext())
+            {
+                Layout.CurrentPlayer = armyContext.Players.Where(p => p.PlayerId == playerId).Single();
+                Layout.AllIslands = armyContext.Islands.Where(i => i.Owner == Layout.CurrentPlayer).ToList();
+                Layout.CurrentIsland = armyContext.Islands.Where(i => i.IslandId == currentIslandId).Single();
+                //Layout.AttackerArmy = armyContext.Armies.Where(a => a.ArmyState == ArmyState.movement && a.Destination == Layout.CurrentIsland).ToList();
+            }
+
+        }
+
+        public LayoutVM Layout { get; set; }
+
+    }
+}
---
+++ ITI.SkyLord/src/ITI.SkyLord/ViewModel/StandardViewModelGeneric.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace ITI.SkyLord.ViewModel
+{
+    public class StandardViewModel<T> : StandardViewModel
+    {
+        public StandardViewModel(int playerId, int currendIslandId) : base (playerId, currendIslandId)
+        {
+        }
+
+
+        T SpecificViewModel { get; set; }
+
+    }
+}
