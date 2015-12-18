7fafe3e0d0bc27ae943cdcfc99baab0b81a644df
Loic <donne@intechinfo.fr>
2015-12-18 18:14:26 +0100

    ça marche plus..... (Voir SeeAllRessources/ SeeRessources)

--- ITI.SkyLord/src/ITI.SkyLord/wwwroot/_references.js
+++ ITI.SkyLord/src/ITI.SkyLord/wwwroot/_references.js
@@ -8,5 +8,5 @@
 /// <reference path="lib/hammer.js/hammer.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
 /// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
-/// <reference path="svg/tour.js" />
+/// <reference path="svg/Tour.js" />
 /// <reference path="ts/testsnap.js" />
