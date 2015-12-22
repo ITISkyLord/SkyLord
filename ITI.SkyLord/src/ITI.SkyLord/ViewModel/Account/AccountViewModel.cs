2ef1ad4e0657bdea204b9c5d6f0d7cca7020b940
KevinRoussel <roussel@intechinfo.fr>
2015-12-22 16:34:47 +0100

    Page de début avec merge des formulaires de connexion et d'inscription

---
+++ ITI.SkyLord/src/ITI.SkyLord/ViewModel/Account/AccountViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ITI.SkyLord.ViewModels.Account;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITI.SkyLord.ViewModel.Account
+{
+    public class AccountViewModel
+    {
+        public RegisterViewModel registerViewModel {get;set;}
+        public LoginViewModel loginViewModel {get;set;}
+
+    }
+}
