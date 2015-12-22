2de2480e4d95ced054bd2aff4eefbdeca49e5f96
Loic <donne@intechinfo.fr>
2015-12-22 18:26:58 +0100

    Début gestion des Message
    Création d'un ManagerMessage
    Création d'un test SendMessage

---
+++ ITI.SkyLord/src/ITI.SkyLord.Tests/EfTests/MessageTest.cs
@@ -0,0 +1,53 @@
+﻿using ITI.SkyLord.Models.Entity_Framework.Contexts;
+using ITI.SkyLord.Models.Managers;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITI.SkyLord.Tests.EfTests
+{
+    [TestFixture]
+    public class MessageTest
+    {
+
+        [Test]
+        //[TestCase("Jesuisunmessagedetest", "Coucou", true )]
+        [TestCase("", "Coucou", false )]
+        [TestCase("Jesuisunmessagedetest", "", false )]
+        [TestCase(" ", "Coucou", false )]
+        [TestCase("Jesuisunmessagedetest", " ", false )]
+        [TestCase("  ", "Coucou", false)]
+        [TestCase("Jesuisunmessagedetest", "  ", false)]
+        public void SendMessage(string coreMessage, string messageObject, bool returnSendMessageExpected)
+        {
+            MessageContext mc = new MessageContext();
+            MessageManager mm = new MessageManager(mc);
+
+            try
+            {
+                Player sender = mc.Players.First(p => p.Name == "LoicD");
+                Player receiver = mc.Players.First(a => a.Name == "Spi");
+
+                var result = mm.SendMessage(sender, receiver, coreMessage, messageObject);
+    
+                Assert.That(result == returnSendMessageExpected);
+                Assert.That(mc.Messages.Where(m => m.CoreMessage == coreMessage && m.MessageObject == messageObject).First() != null);
+                
+            }
+            finally
+            {
+                var t = mc.Messages.Where(m => m.CoreMessage == coreMessage).FirstOrDefault();
+                if (t != null) mc.Messages.Remove(t);
+                mc.SaveChanges();
+                mc.Dispose();
+            }
+        }
+
+
+
+    }
+
+
+}
---
+++ ITI.SkyLord/src/ITI.SkyLord/Models/Managers/MessageManager.cs
@@ -0,0 +1,60 @@
+﻿using ITI.SkyLord.Models.Entity_Framework.Contexts;
+using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace ITI.SkyLord.Models.Managers
+{
+    public class MessageManager
+    {
+        private MessageContext messageContext { get; }
+        public MessageManager(MessageContext mc)
+        {
+            messageContext = mc;
+        }
+        public bool SendMessage(Player sender, Player receiver, string CoreMessage, string ObjectMessage)
+        {
+            bool success = false;
+
+            if(String.IsNullOrWhiteSpace(CoreMessage) || String.IsNullOrWhiteSpace(ObjectMessage) || String.IsNullOrEmpty(CoreMessage) || String.IsNullOrEmpty(ObjectMessage))
+            {
+                Console.WriteLine("Vos données sont incorrectes.");
+                return success;
+            }
+            Message message = new Message { Receiver = receiver, Sender = sender, CoreMessage = CoreMessage, MessageObject = ObjectMessage };
+            messageContext.Add(message);
+            messageContext.SaveChanges();
+            success = true;
+
+            return success;
+        }
+
+        public IList<Message> GetAllMessage(Player player)
+        {
+            throw new NotImplementedException();
+        }
+        public bool AnswerMessage(Message message, string CoreMessage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteMessage(Message message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteMessage(long messageId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Message> GetAllUnreadMessage(Player player)
+        {
+            throw new NotImplementedException();
+
+        }
+    }
+}
