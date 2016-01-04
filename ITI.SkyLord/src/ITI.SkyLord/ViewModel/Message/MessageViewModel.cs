eb95888963875b0c291d33bd5c54267e7b89ab37
Loic <donne@intechinfo.fr>
2016-01-04 09:16:36 +0100

    Read AllMessages fais

---
+++ ITI.SkyLord/src/ITI.SkyLord/ViewModel/Message/MessageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITI.SkyLord.ViewModel
+{
+    public class MessageViewModel : StandardViewModel
+    {
+       public List<Message> AllMessages { get; set; }
+        public Message Message { get; set; }
+    }
+}
