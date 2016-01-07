baa69444c4e3c5d3ed9b3ac1623a4ab7c29987e5
Tristan Letrou <letrou@intechinfo.fr>
2016-01-07 12:21:27 +0100

    AddBuildingToIsland OK
    Reste à faire : vérifier le nombre d'emplacements disponibles

--- ITI.SkyLord/src/ITI.SkyLord/Services/BuildingManager.cs
+++ ITI.SkyLord/src/ITI.SkyLord/Services/BuildingManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ITI.SkyLord.Models.Entity_Framework.Contexts;
+using Microsoft.Data.Entity;
 
 namespace ITI.SkyLord.Services
 {
@@ -33,10 +34,14 @@ namespace ITI.SkyLord.Services
                 BuildingName = buildingName,
                 Level = _currentContext.BuildingLevels.Where( bl => bl.BuildingName == buildingName && bl.Number == 1 ).Single()
             };
-            _currentContext.Add( buildingToAdd );
+
+            // If the building already exists AND is supposed to be unique on the island, don't add it and return false
+            if ( GetBuildingsOnCurrentIsland().Any( b => buildingToAdd.BuildingName == b.BuildingName ) && IsBuildingUnique( buildingName ) )
+            {
+                return false;
+            }
 
             Island currentIsland = _currentContext.GetIsland( _currentIslandId, _playerId);
-
             if( currentIsland.Buildings == null )
             {
                 currentIsland.Buildings = new List<Building>();
@@ -60,9 +65,76 @@ namespace ITI.SkyLord.Services
         }
         private string BuildingNameToName( BuildingName buildingName )
         {
-            Random rnd = new Random();
-            int month = rnd.Next(1, 13);
-            return "test" + month ;
+            string name;
+            switch( buildingName )
+            {
+                case BuildingName.academy:
+                    name = "Académie";
+                    break;
+                case BuildingName.barrack:
+                    name = "Baraquement";
+                    break;
+                case BuildingName.cache:
+                    name = "Cachette";
+                    break;
+                case BuildingName.cristalField:
+                    name = "Mine de cristal";
+                    break;
+                case BuildingName.invocation:
+                    name = "Cercle d'invocation";
+                    break;
+                case BuildingName.laboratory:
+                    name = "Laboratoire";
+                    break;
+                case BuildingName.library:
+                    name = "Bibliothèque";
+                    break;
+                case BuildingName.magicField:
+                    name = "Champ de magie";
+                    break;
+                case BuildingName.metalField:
+                    name = "Mine de métal";
+                    break;
+                case BuildingName.shield:
+                    name = "Bouclier";
+                    break;
+                case BuildingName.tower:
+                    name = "Tour de Mage";
+                    break;
+                case BuildingName.woodField:
+                    name = "Camp de bucherons";
+                    break;
+                default :
+                    name = "Error";
+                    break;
+            }
+            return name;
+        }
+
+        private bool IsBuildingUnique( BuildingName buildingName )
+        {
+            bool isUnique;
+            switch ( buildingName )
+            {
+                case BuildingName.cristalField :
+                case BuildingName.magicField :
+                case BuildingName.metalField :
+                case BuildingName.woodField :
+                    isUnique = false;
+                    break;
+
+                default:
+                    isUnique = true;
+                    break;
+            }
+            return isUnique;
+        }
+
+        private List<Building> GetBuildingsOnCurrentIsland()
+        {
+            return _currentContext.Islands
+                    .Include( i => i.Buildings )
+                    .Where( i => i.IslandId == _currentIslandId ).SingleOrDefault().Buildings.ToList();
         }
     }
 }
