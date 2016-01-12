b1b5993c7ae2c57f255d96e00a2061b7437c6cf2
KevinRoussel <roussel@intechinfo.fr>
2016-01-12 16:36:57 +0100

    View WorldMap avec les contraintes et de nouvelles images

--- ITI.SkyLord/src/ITI.SkyLord/wwwroot/ts/worldmap.ts
+++ ITI.SkyLord/src/ITI.SkyLord/wwwroot/ts/worldmap.ts
@@ -16,13 +16,56 @@ var taillemap: number = 500;
 
 // Main
 $(document).ready(function () {
-    // On rend l'élément draggable
-    $("#draggable").draggable();
-
+    
     // Centre la map sur l'island de la personne
     var pos = GetCoordinate($("#MyIsland"));
     $("#draggable").css("top", pos.X + "px");
     $("#draggable").css("left", pos.Y + "px");
+    TestConstraint($("#draggable"));
+
+    // On rend l'élément draggable
+    $("#draggable").draggable(
+        {
+
+            drag: function (event) {
+                
+                return TestConstraint($(this));
+            }
+        });
+
+    function TestConstraint(node: JQuery) {
+        var pos_x: number = parseInt($(node).css("top"));
+        var pos_y: number = parseInt($(node).css("left"));
+
+        if (pos_x >= 0 && pos_y >= 0) {
+            $(node).css("top", "-1px");
+            $(node).css("left", "-1px");
+            return false;
+        }
+        else if (pos_x < -5900 || pos_y < -5900) {
+            $(node).css("top", "-5899px");
+            $(node).css("left", "-5899px");
+            return false;
+        }
+        else if (pos_x < -5900) {
+            $(node).css("top", "-5899px");
+            return false;
+        }
+        else if (pos_y < -5900) {
+            $(node).css("left", "-5899px");
+            return false;
+        }
+        else if (pos_x >= 0) {
+            $(node).css("top", "-1px");
+            return false;
+        }
+        else if (pos_y >= 0) {
+            $(node).css("left", "-1px");
+            return false;
+        }
+
+    }
+
 
     // Récupère le node du menu quand on clique sur une island
     var menu: JQuery = $("#Menu");
