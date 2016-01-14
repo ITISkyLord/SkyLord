7335c42e44cdd9fe5028e7d610551fc92d8bf47e
KevinRoussel <roussel@intechinfo.fr>
2016-01-14 11:00:09 +0100

    Placement des buildings sur l'island et affichage de l'interface qui va bien en fonction du batiment cliqué

--- ITI.SkyLord/src/ITI.SkyLord/wwwroot/js/seeIsland.js
+++ ITI.SkyLord/src/ITI.SkyLord/wwwroot/js/seeIsland.js
@@ -1,2 +1,14 @@
 /// <reference path="jquery.d.ts" />
 /// <reference path="jquery-ui.d.ts" />
+$(document).ready(function () {
+    // Quand on selectionne un building => On affiche l'interface qui va bien
+    $(".Building").click(function (event) {
+        var buildingname = $(this).attr("buildingname");
+        var viewselector = "#ui_" + $(this).attr("templateid");
+        $(".templatebuilding").fadeOut("fast", function () {
+            setTimeout(function () {
+                $(viewselector).fadeIn();
+            }, 200);
+        });
+    });
+});
