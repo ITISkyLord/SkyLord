514e293fa0aee5784d2742723c804d12660417b4
Aymeric <richard.aymeric.92@gmail.com>
2016-01-24 06:06:06 +0100

    Intégration de l'intégration continue.

---
+++ ITI.SkyLord/src/CodeCakeBuilder/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Code.Cake;
+using CodeCake;
+using Microsoft.Extensions.PlatformAbstractions;
+
+namespace CodeCakeBuilder
+{
+    public static class Program
+    {
+        public static int Main( string[] args )
+        {
+            string solutionDir = Path.GetDirectoryName( PlatformServices.Default.Application.ApplicationBasePath );
+            var app = new CodeCakeApplication( solutionDir, typeof(Program).Assembly );
+            bool interactive = !args.Contains( '-' + InteractiveAliases.NoInteractionArgument, StringComparer.OrdinalIgnoreCase );
+            int result = app.Run( args );
+            Console.WriteLine();
+            if( interactive )
+            {
+                Console.WriteLine( "Hit any key to exit. (Use -{0} parameter to exit immediately)", InteractiveAliases.NoInteractionArgument );
+                Console.ReadKey();
+            }
+            return result;
+        }
+    }
+}
