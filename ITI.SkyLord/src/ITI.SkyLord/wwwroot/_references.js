72665cb8549465ba96ef25336452502b1e16be9e
KevinRoussel <roussel@intechinfo.fr>
2016-01-26 19:52:06 +0100

    Style WorldMap Menu

--- ITI.SkyLord/src/ITI.SkyLord/wwwroot/_references.js
+++ ITI.SkyLord/src/ITI.SkyLord/wwwroot/_references.js
@@ -13,4 +13,3 @@
 /// <reference path="lib/jquery/dist/jquery.js" />
 /// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
 /// <reference path="svg/Tour.js" />
-/// <reference path="ts/timemanager.js" />
