742bd993fc46ac1b6a9f3a511aa40a8207752934
KevinRoussel <roussel@intechinfo.fr>
2016-01-26 11:12:29 +0100

    fix timeManager

--- ITI.SkyLord/src/ITI.SkyLord/wwwroot/js/timeManager.js
+++ ITI.SkyLord/src/ITI.SkyLord/wwwroot/js/timeManager.js
@@ -1,26 +1,18 @@
-﻿/// <reference path="jquery.d.ts" />
-
+/// <reference path="jquery.d.ts" />
 $(document).ready(function () {
-
     setInterval(function () {
-        $(".time").each(function (key: number, value: Element) {
+        $(".time").each(function (key, value) {
             console.log("update");
-
             var node = $(value);
-
             DecresingTime(node);
             VerboseTime(node);
         });
     }, 1000);
-
 });
- 
-function DecresingTime(node: JQuery) {
-
-    var secondes: number = +$(node).attr("secondes");
-    var minutes: number = +$(node).attr("minutes");
-    var hours: number = +$(node).attr("hours");
-
+function DecresingTime(node) {
+    var secondes = +$(node).attr("secondes");
+    var minutes = +$(node).attr("minutes");
+    var hours = +$(node).attr("hours");
     if (secondes == 0 && minutes == 0 && hours == 0) {
         $(node).removeClass("time");
         return;
@@ -37,17 +29,13 @@ function DecresingTime(node: JQuery) {
     if (hours < 0) {
         hours = 0;
     }
-
     $(node).attr("secondes", secondes);
     $(node).attr("minutes", minutes);
     $(node).attr("hours", hours);
-
 }
-
-function VerboseTime(node: JQuery) {
-    var secondes: number = +$(node).attr("secondes");
-    var minutes: number = +$(node).attr("minutes");
-    var hours: number = +$(node).attr("hours");
-
+function VerboseTime(node) {
+    var secondes = +$(node).attr("secondes");
+    var minutes = +$(node).attr("minutes");
+    var hours = +$(node).attr("hours");
     $(node).text((hours < 10 ? "0" + hours : hours) + ":" + (minutes < 10 ? "0" + minutes : minutes) + ":" + (secondes < 10 ? "0" + secondes : secondes));
 }
